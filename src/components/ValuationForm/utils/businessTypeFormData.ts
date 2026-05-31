81b5876cf15971ee3457b1c294ee5f1b5cc1be12
mandiau-matthias <mmandiau@gmail.com>
2026-05-31 15:59:43 +0200

    Update businessTypeFormData.ts

--- src/components/ValuationForm/utils/businessTypeFormData.ts
+++ src/components/ValuationForm/utils/businessTypeFormData.ts
@@ -1,5 +1,6 @@
 import type { BusinessType } from '../../../services/businessTypesApi'
 import type { ValuationFormData } from '../../../types/valuation'
+import { normalizeBusinessTypeId } from '../../../utils/businessTypeIdAliases'
 
 type BusinessTypeFormSource = Pick<BusinessType, 'id'> & Partial<BusinessType>
 
@@ -8,11 +9,12 @@ export function buildBusinessTypeFormData(
   fallbackIndustry = 'services'
 ): Partial<ValuationFormData> {
   const industry = businessType.industry || businessType.industryMapping || fallbackIndustry
+  const businessTypeId = normalizeBusinessTypeId(businessType.id) ?? businessType.id
 
   return {
-    business_type_id: businessType.id,
+    business_type_id: businessTypeId,
     business_type_title: businessType.title,
-    business_model: businessType.id,
+    business_model: businessTypeId,
     industry,
     subIndustry: businessType.category,
     _internal_dcf_preference: businessType.dcfPreference,
