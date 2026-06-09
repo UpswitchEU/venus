adab0c0e9fd436ed7144f7c75fe8fe948705ce4a
mandiau-matthias <mmandiau@gmail.com>
2026-06-09 14:56:57 +0200

    .

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   /* Shared settings for all the projects below */
   use: {
     /* Base URL to use in actions like `await page.goto('/')` */
-    baseURL: process.env.BASE_URL || 'http://localhost:3001',
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:3001',
     
     /* Collect trace when retrying the failed test */
     trace: 'on-first-retry',
@@ -94,8 +94,8 @@ export default defineConfig({
   
   /* Run your local dev server before starting the tests */
   webServer: process.env.CI ? undefined : {
-    command: 'npm run dev',
-    url: 'http://localhost:3001',
+    command: 'pnpm run dev',
+    url: 'http://127.0.0.1:3001',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
